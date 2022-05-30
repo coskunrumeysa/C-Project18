5f2c299ec9acd6e8fb47111a84e8572cfe13ec2b
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-30 14:30:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Beni Yaratn Elbet Yolumu Gösterir .Şuara78
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char deger;
+            deger = 'r';
+            char value;
+
+            Console.WriteLine("Your character:" + " " + deger);
+            Console.WriteLine("Another Example:");
+            Console.WriteLine();
+            Console.WriteLine("Please enter character:");
+            value = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Your entried character is:"+" "+value);
+            Console.Read();
+
+
+            
+        }
+
+
+    }
+}
